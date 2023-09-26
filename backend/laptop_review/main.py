b6a8f677a6da854a90db42bdbab27465fe8e78d3
Dikshant <dikkpsd@gmail.com>
2023-09-26 18:31:27 +0530

    Added Fastapi

--- backend/laptop_review/main.py
+++ backend/laptop_review/main.py
@@ -1,16 +1,32 @@
 import firebase_admin
 from firebase_admin import db
-
-
-cred = firebase_admin.credentials.Certificate('/home/dikshant/Projects/Milan-Project/laptop_review/service.json')
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+load_dotenv()
+cred = firebase_admin.credentials.Certificate('/home/dikshant/Projects/Milan-Project/backend/laptop_review/service.json')
 
 firebase_admin.initialize_app(cred,
- { 'databaseURL': 'https://laptops-39b36-default-rtdb.firebaseio.com/'})
+ { 'databaseURL': os.getenv('DATABASE_URL')})
 
 ref = db.reference('/')
 
+app = FastAPI()
 
-import json
-with open("book.json", "r") as f:
-	file_contents = json.load(f)
-ref.set(file_contents)
+origins = [
+    "http://localhost",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+def route():
+    return {"message" : "hello world"}
