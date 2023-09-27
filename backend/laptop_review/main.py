c44476bef5e7f4e9f812576567211d469c42f5de
Dikshant <dikkpsd@gmail.com>
2023-09-27 13:25:45 +0530

    Added Firestore compatibility and Score predictor model

--- backend/laptop_review/main.py
+++ backend/laptop_review/main.py
@@ -1,16 +1,21 @@
 import firebase_admin
-from firebase_admin import db
+from firebase_admin import firestore, credentials
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import tensorflow as tf
+import pandas as pd
+import ktrain
+
 load_dotenv()
-cred = firebase_admin.credentials.Certificate(os.path.dirname(__file__) + '/service.json')
 
-firebase_admin.initialize_app(cred,
- { 'databaseURL': os.getenv('DATABASE_URL')})
+cred = credentials.Certificate(os.path.dirname(__file__) + '/service.json')
+firebase_admin.initialize_app(cred)
 
-ref = db.reference('/')
+
+db = firestore.client()
+
 
 app = FastAPI()
 
@@ -27,6 +32,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# positive and negative score predictor
+predictor = ktrain.load_predictor('models')
+
+# checking all the collections in database
+collections = db.collections()
+for collection in collections:
+    print(collection.id)
+
+
 @app.get('/')
-def route():
-    return {"message" : "hello world"}
+async def route():
+    return {"message" :  'Still working' }
