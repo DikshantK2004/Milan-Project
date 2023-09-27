9d2c2ba6b40363a0430b3d1efd39551836dc15e8
jash-j3 <jash.jhatakia@gmail.com>
2023-09-28 00:31:48 +0530

    added submit button

--- laptop-reviews/src/Product.jsx
+++ laptop-reviews/src/Product.jsx
@@ -10,37 +10,39 @@ import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import useAuth from "./useAuth";
 import { signOut, getAuth } from "firebase/auth";
-import {app} from './firebase.config'
+import { app } from "./firebase.config";
+import Button from '@mui/material/Button';
 
 async function dataReturn() {
-  const fetchData = await fetch("http://localhost:8000/MacBook Pro", {"method" : "GET"});
+  const fetchData = await fetch("http://localhost:8000/MacBook Pro", {
+    method: "GET",
+  });
 
   const dataRes = await fetchData.json();
 
   return dataRes;
 }
 
-
 function Product() {
-  const user = useAuth()
-  const auth = getAuth(app)
-  if(user)
-  {
-  user.getIdToken()
-    .then((authToken) => {
-      console.log('Authentication token:', authToken);
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
+  const user = useAuth();
+  const auth = getAuth(app);
+  if (user) {
+    user
+      .getIdToken()
+      .then((authToken) => {
+        console.log("Authentication token:", authToken);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
   }
-    const [review, setReview] = useState("")
-    const handler = async(event) =>{
-        event.preventDefault();
-        setReview(event.target.value);
-      }
+  const [review, setReview] = useState("");
+  const handler = async (event) => {
+    event.preventDefault();
+    setReview(event.target.value);
+  };
   const [nul, setNul] = useState(0);
-  const [Data, setData] = useState({alert:true,positive:[],negative:[]});
+  const [Data, setData] = useState({ alert: true, positive: [], negative: [] });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,54 +53,60 @@ function Product() {
     fetchData();
   }, []);
 
-  
-  return(
-    
-            <div className="product">
-            <img
-              className="lappy"
-              src={require("./assets/air.png")}
-              alt="laptop-img"
-            />
-            <h1>Apple Air M1</h1>
-            <h4>Reviews</h4>
-            <div className="prorev">
-              <div className="pro-div">
-                <h4>Positive</h4>
-                <div className="prod-rev">{Data.positive.map((item)=>(<ReviewComponent
-              review={item.review}
-              score={item.score}
-              username={item.username}
-              date={item.date}
-            />))}</div>
-            </div>
-            <div className="prodprog">
-              <Stack spacing={2}>
-                <CircularProgress
-                  size="lg"
-                  determinate
-                  value={70}
-                  sx={{ "--CircularProgress-size": "150px" }}
-                >
-                  <Typography sx={{ fontSize: "30px", fontWeight: "700" }}>
-                    70%
-                  </Typography>
-                </CircularProgress>
-              </Stack>
-              <h4>Users thought this Laptop is a good Buy</h4>
-            </div>
-            <div className="pro-div">
-              <h4>Negative</h4>
-              <div className="prod-rev">{Data.negative.map((item)=>(<ReviewComponent
-              review={item.review}
-              score={item.score}
-              username={item.username}
-              date={item.date}
-            />))}</div>
-            </div>
+  return (
+    <div className="product">
+      <img
+        className="lappy"
+        src={require("./assets/air.png")}
+        alt="laptop-img"
+      />
+      <h1>Apple Air M1</h1>
+      <h4>Reviews</h4>
+      <div className="prorev">
+        <div className="pro-div">
+          <h4>Positive</h4>
+          <div className="prod-rev">
+            {Data.positive.map((item) => (
+              <ReviewComponent
+                review={item.review}
+                score={item.score}
+                username={item.username}
+                date={item.date}
+              />
+            ))}
           </div>
+        </div>
+        <div className="prodprog">
+          <Stack spacing={2}>
+            <CircularProgress
+              size="lg"
+              determinate
+              value={70}
+              sx={{ "--CircularProgress-size": "150px" }}
+            >
+              <Typography sx={{ fontSize: "30px", fontWeight: "700" }}>
+                70%
+              </Typography>
+            </CircularProgress>
+          </Stack>
+          <h4>Users thought this Laptop is a good Buy</h4>
+        </div>
+        <div className="pro-div">
+          <h4>Negative</h4>
+          <div className="prod-rev">
+            {Data.negative.map((item) => (
+              <ReviewComponent
+                review={item.review}
+                score={item.score}
+                username={item.username}
+                date={item.date}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
 
-          <div className="inputreview">
+      <div className="inputreview">
         <h4>Tell us what you feel about this Laptop?</h4>
         <div className="reviewInput">
           <Box
@@ -122,10 +130,10 @@ function Product() {
             </div>
           </Box>
         </div>
+        <Button variant="outlined" sx={{color:"black"}}>Submit</Button>
       </div>
-        </div>)
-            
-          
+    </div>
+  );
 }
 
 export default Product;
