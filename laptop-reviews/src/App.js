7de8cea0c1fc51c04c607584aa716fb20aa7b834
Himanshu Sekhar <hsmaharaj1@gmail.com>
2023-09-27 17:17:52 +0530

    login system updated

--- laptop-reviews/src/App.js
+++ laptop-reviews/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import LoginPage from './LoginPage';
 import HomePage from './HomePage';
+import SignUp from './SignUp';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage/>}></Route>
           <Route path="/login" element={<LoginPage/>}></Route>
+          <Route path="/signup" element={<SignUp/>}></Route>
         </Routes>
       </Router>
   );
--- laptop-reviews/src/LoginPage.jsx
+++ laptop-reviews/src/LoginPage.jsx
@@ -5,17 +5,38 @@ import {
     getAuth,
     GoogleAuthProvider,
     signInWithPopup,
-    signOut
+    signOut,
+    signInWithEmailAndPassword
 } from "firebase/auth"
-import{useState, useEffect} from "react"
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom";
 
 const provider = new GoogleAuthProvider();
-const auth = getAuth();
+const auth = getAuth(app);
 
-function LoginPage(){
-    function handleLogin(event){
+function LoginPage() {
+    const navigate = useNavigate()
+    const [mail, setMail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(null)
+
+    //Sign in with email and password
+    const signIn = () => {
+        signInWithEmailAndPassword(auth, mail, password)
+            .then((userCredential) => {
+                const user = userCredential.user
+                console.log("Signned in successfully", user)
+            })
+            .catch((error) => {
+                setError(error)
+                console.log(error)
+            })
+    }
+
+    //Sign in with Google
+    function handleLogin(event) {
         signInWithPopup(auth, provider)
-            .then(async(result) => {
+            .then(async (result) => {
                 const credentials = GoogleAuthProvider.credentialFromResult(result)
                 const token = credentials.accessToken
                 const user = result.user
@@ -29,8 +50,24 @@ function LoginPage(){
 
     return (
         <div className="loginPage">
+            {error && <div className="error-msg">{error.message}</div>}
+            <div className="pagename">
+                SIGN IN
+            </div>
+            <div className="login-box">
+                <input type="text" id="mail-box" placeholder="E-mail" value={mail} onChange={(event) => (setMail(event.target.value))} />
+                <input type="password" id="password-box" placeholder="Password" value={password} onChange={(event) => (setPassword(event.target.value))} />
+                <div className="forgot-pass-link">
+                    <a href="" id="new-link">Forgot password?</a>
+                </div>
+                <input type="submit" value="Sign in" id="signin-btn" onClick={signIn} />
+                <div className="new-acnt-text">
+                    New here?
+                    <span onClick={() => {navigate("/signup")}} id="new-link">  Create an account</span>
+                </div>
+            </div>
             <div className="google-signin-btn" onClick={handleLogin}>
-                <img src={googleIcon} id="google-icon"/>
+                <img src={googleIcon} id="google-icon" />
                 Sign in with Google
             </div>
         </div>
