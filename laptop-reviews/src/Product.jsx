5109400681a105728c92ef54c2774a781c559df4
jash-j3 <jash.jhatakia@gmail.com>
2023-09-27 17:50:48 +0530

    Made a component of review

--- laptop-reviews/src/Product.jsx
+++ laptop-reviews/src/Product.jsx
@@ -1,26 +1,9 @@
 import React from "react";
 import "./Product.css";
-import Box from "@mui/material/Box";
-import Rating from "@mui/material/Rating";
-import StarIcon from "@mui/icons-material/Star";
 import Stack from "@mui/joy/Stack";
 import Typography from "@mui/joy/Typography";
 import CircularProgress from "@mui/joy/CircularProgress";
-
-const labels = {
-  0.5: "Useless",
-  1: "Useless+",
-  1.5: "Poor",
-  2: "Poor+",
-  2.5: "Ok",
-  3: "Ok+",
-  3.5: "Good",
-  4: "Good+",
-  4.5: "Excellent",
-  5: "Excellent+",
-};
-
-const value = 4.5;
+import ReviewComponent from "./ReviewComponent";
 
 function Product() {
   return (
@@ -36,84 +19,13 @@ function Product() {
         <div className="pro-div">
           <h4>Positive</h4>
           <div className="prod-rev">
-            <div className="item3">
-              <div className="boxdiv">
-                <Box
-                  sx={{
-                    width: 200,
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <Rating
-                    name="text-feedback"
-                    value={value}
-                    readOnly
-                    precision={0.5}
-                    emptyIcon={
-                      <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
-                    }
-                  />
-                  <Box sx={{ ml: 2 }}>{labels[value]}</Box>
-                </Box>
-              </div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ut
-              ullam. Corrupti autem, ratione laudantium quidem fuga possimus
-              voluptas deserunt atque nostrum aperiam pariatur esse reiciendis
-              velit nulla, numquam quas dolorum dicta nesciunt maxime explicabo
-              asperiores tempore! Aspernatur possimus illo eum corporis rerum in
-              ex consectetur aut fuga vero? Sunt, maiores? Minus delectus
-              inventore laborum laudantium alias eaque minima asperiores.
-              <br />
-              <span>
-                <span style={{ textAlign: "center", fontSize: "13px" }}>
-                  - Jcube
-                </span>
-                <br />
-                <span style={{ textAlign: "center", fontSize: "13px" }}>
-                  24/09/23 1775 hrs
-                </span>
-              </span>
-            </div>
-            <div className="item3">
-              <div className="boxdiv">
-                <Box
-                  sx={{
-                    width: 200,
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <Rating
-                    name="text-feedback"
-                    value={value}
-                    readOnly
-                    precision={0.5}
-                    emptyIcon={
-                      <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
-                    }
-                  />
-                  <Box sx={{ ml: 2 }}>{labels[value]}</Box>
-                </Box>
-              </div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ut
-              ullam. Corrupti autem, ratione laudantium quidem fuga possimus
-              voluptas deserunt atque nostrum aperiam pariatur esse reiciendis
-              velit nulla, numquam quas dolorum dicta nesciunt maxime explicabo
-              asperiores tempore! Aspernatur possimus illo eum corporis rerum in
-              ex consectetur aut fuga vero? Sunt, maiores? Minus delectus
-              inventore laborum laudantium alias eaque minima asperiores.
-              <br />
-              <span>
-                <span style={{ textAlign: "center", fontSize: "13px" }}>
-                  - Jcube
-                </span>
-                <br />
-                <span style={{ textAlign: "center", fontSize: "13px" }}>
-                  24/09/23 1775 hrs
-                </span>
-              </span>
-            </div>
+            <ReviewComponent
+              review="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi est exercitationem dolores, libero mollitia quis alias minima tempore in ullam, temporibus consequuntur esse laudantium optio deleniti blanditiis ab voluptatum dolorem! Neque repellat, fugiat modi beatae mollitia fugit maxime laudantium molestiae ut dolores cupiditate accusantium officia totam id possimus unde ipsam fuga nemo dicta. Sint, eaque nihil. Harum voluptates perspiciatis distinctio?
+            "
+              score="4.5"
+              username="Jcube"
+              date="17/09/23"
+            />
           </div>
         </div>
         <div className="prodprog">
@@ -134,84 +46,6 @@ function Product() {
         <div className="pro-div">
           <h4>Negative</h4>
           <div className="prod-rev">
-            <div className="item3">
-              <div className="boxdiv">
-                <Box
-                  sx={{
-                    width: 200,
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <Rating
-                    name="text-feedback"
-                    value={value}
-                    readOnly
-                    precision={0.5}
-                    emptyIcon={
-                      <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
-                    }
-                  />
-                  <Box sx={{ ml: 2 }}>{labels[value]}</Box>
-                </Box>
-              </div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ut
-              ullam. Corrupti autem, ratione laudantium quidem fuga possimus
-              voluptas deserunt atque nostrum aperiam pariatur esse reiciendis
-              velit nulla, numquam quas dolorum dicta nesciunt maxime explicabo
-              asperiores tempore! Aspernatur possimus illo eum corporis rerum in
-              ex consectetur aut fuga vero? Sunt, maiores? Minus delectus
-              inventore laborum laudantium alias eaque minima asperiores.
-              <br />
-              <span>
-                <span style={{ textAlign: "center", fontSize: "13px" }}>
-                  - Jcube
-                </span>
-                <br />
-                <span style={{ textAlign: "center", fontSize: "13px" }}>
-                  24/09/23 1775 hrs
-                </span>
-              </span>
-            </div>
-            <div className="item3">
-              <div className="boxdiv">
-                <Box
-                  sx={{
-                    width: 200,
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <Rating
-                    name="text-feedback"
-                    value={value}
-                    readOnly
-                    precision={0.5}
-                    emptyIcon={
-                      <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
-                    }
-                  />
-                  <Box sx={{ ml: 2 }}>{labels[value]}</Box>
-                </Box>
-              </div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ut
-              ullam. Corrupti autem, ratione laudantium quidem fuga possimus
-              voluptas deserunt atque nostrum aperiam pariatur esse reiciendis
-              velit nulla, numquam quas dolorum dicta nesciunt maxime explicabo
-              asperiores tempore! Aspernatur possimus illo eum corporis rerum in
-              ex consectetur aut fuga vero? Sunt, maiores? Minus delectus
-              inventore laborum laudantium alias eaque minima asperiores.
-              <br />
-              <span>
-                <span style={{ textAlign: "center", fontSize: "13px" }}>
-                  - Jcube
-                </span>
-                <br />
-                <span style={{ textAlign: "center", fontSize: "13px" }}>
-                  24/09/23 1775 hrs
-                </span>
-              </span>
-            </div>
             <button></button>
           </div>
         </div>
---
+++ laptop-reviews/src/ReviewComponent.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import StarIcon from "@mui/icons-material/Star";
+
+const labels = {
+    0.5: "Useless",
+    1: "Useless+",
+    1.5: "Poor",
+    2: "Poor+",
+    2.5: "Ok",
+    3: "Ok+",
+    3.5: "Good",
+    4: "Good+",
+    4.5: "Excellent",
+    5: "Excellent+",
+  };
+  
+  const value = 4.5;
+
+function ReviewComponent(props) {
+  return (
+    <div>
+      <div className="item3">
+        <div className="boxdiv">
+          <Box
+            sx={{
+              width: 200,
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Rating
+              name="text-feedback"
+              value={props.score}
+              readOnly
+              precision={0.5}
+              emptyIcon={
+                <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+              }
+            />
+            <Box sx={{ ml: 2 }}>{labels[value]}</Box>
+          </Box>
+        </div>
+        {props.review}
+        <br />
+        <span>
+          <span style={{ textAlign: "center", fontSize: "13px" }}>- {props.username}</span>
+          <br />
+          <span style={{ textAlign: "center", fontSize: "13px" }}>
+            {props.date}
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default ReviewComponent;
